d2ebba4443c24564aa8a184538e76efddd5abb4c
vagrant <vagrant@vagrant.vm>
2021-06-01 07:18:41 +0000

    feat: implement quick sort and unittest

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -18,3 +18,14 @@ class TestSort(unittest.TestCase):
         collection = random.choices(string.ascii_letters + string.digits, k=100)
         self.assertEqual(self.sort.bubble_sort(collection), sorted(collection))
 
+    def test_quick_sort(self):
+        self.assertEqual(self.sort.quick_sort([0, 5, 2, 3, 2]), [0, 2, 2, 3, 5])
+        self.assertEqual(self.sort.quick_sort([]), [])
+        self.assertEqual(self.sort.quick_sort([-2, -8, -5]), [-8, -5, -2])
+        self.assertEqual(self.sort.quick_sort(['d', 'a', 'b', 'e', 'c']), ['a', 'b', 'c', 'd', 'e'])
+        collection1 = random.sample(range(-50, 50), 100)
+        collection2 = collection1.copy()
+        self.assertEqual(self.sort.quick_sort(collection1), sorted(collection2))
+        collection1 = random.choices(string.ascii_letters + string.digits, k=100)
+        collection2 = collection1.copy()
+        self.assertEqual(self.sort.quick_sort(collection1), sorted(collection2))
