b4cfa1e6fca09046a86c9fd60299807d0948e641
jiangpancjy <jiangpancjy@163.com>
2021-06-01 12:26:01 +0000

    feat: implememt selection sort and unittest

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -29,3 +29,13 @@ class TestSort(unittest.TestCase):
         collection1 = random.choices(string.ascii_letters + string.digits, k=100)
         collection2 = collection1.copy()
         self.assertEqual(self.sort.quick_sort(collection1), sorted(collection2))
+
+    def test_selection_sort(self):
+        self.assertEqual(self.sort.selection_sort([0, 5, 2, 3, 2]), [0, 2, 2, 3, 5])
+        self.assertEqual(self.sort.selection_sort([]), [])
+        self.assertEqual(self.sort.selection_sort([-2, -8, -5]), [-8, -5, -2])
+        self.assertEqual(self.sort.selection_sort(['d', 'a', 'b', 'e', 'c']), ['a', 'b', 'c', 'd', 'e'])
+        collection = random.sample(range(-50, 50), 100)
+        self.assertEqual(self.sort.selection_sort(collection), sorted(collection))
+        collection = random.choices(string.ascii_letters + string.digits, k=100)
+        self.assertEqual(self.sort.selection_sort(collection), sorted(collection))
