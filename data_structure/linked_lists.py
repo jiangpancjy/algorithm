6231088499b517cde8177fcef0c0f878d51b525f
jiangpancjy <jiangpancjy@163.com>
2021-07-01 03:44:39 +0000

    feat: add insert, remove, pop method to DoubleLinkedList

--- data_structure/linked_lists.py
+++ data_structure/linked_lists.py
@@ -22,10 +22,10 @@ class LinkedList:
         else:
             seq_length = len(seq)
             self._length = seq_length
-            self.head.next = curr = Node(value=seq[0])
+            self.head.next = current_node = Node(value=seq[0])
             for i in range(1, seq_length):
-                curr.next = Node(value=seq[i])
-                curr = curr.next
+                current_node.next = Node(value=seq[i])
+                current_node = current_node.next
 
     def __getitem__(self, pos):
         self._check_pos(pos)
@@ -39,20 +39,20 @@ class LinkedList:
         return self._length
 
     def __str__(self):
-        curr = self.head
+        current_node = self.head
         string = "【head】"
-        while curr.next is not None:
-            curr = curr.next
-            string += f"->【{curr.value}】"
+        while current_node.next is not None:
+            current_node = current_node.next
+            string += f"->【{current_node.value}】"
         return string
 
     def __iter__(self):
         i = 0
-        curr = self.head
+        current_node = self.head
         while i < self._length:
-            curr = curr.next
+            current_node = current_node.next
             i += 1
-            yield curr
+            yield current_node
 
     def _check_pos(self, pos, method_type=MethodType.INDEX):
         if not isinstance(pos, int):
@@ -72,11 +72,11 @@ class LinkedList:
         """
         pos = self._convert_negative_2_positive(pos)
         i = 0
-        prev = self.head
+        previous_node = self.head
         while i < pos:
-            prev = prev.next
+            previous_node = previous_node.next
             i += 1
-        return prev
+        return previous_node
 
     def _convert_negative_2_positive(self, pos):
         """
@@ -104,9 +104,9 @@ class LinkedList:
         """
         self._check_pos(pos, method_type=MethodType.INSERT)
         node = Node(value=value)
-        prev = self._traverse(pos)
-        node.next = prev.next
-        prev.next = node
+        previous_node = self._traverse(pos)
+        node.next = previous_node.next
+        previous_node.next = node
         self._length += 1
 
     def remove(self, value):
@@ -114,14 +114,14 @@ class LinkedList:
         移除与值 value 匹配的第一个节点
         :param value: 要移除节点的值
         """
-        prev = self.head
+        previous_node = self.head
         for node in self:
             if node.value == value:
-                prev.next = node.next
+                previous_node.next = node.next
                 node.next = None
                 self._length -= 1
                 return
-            prev = node
+            previous_node = node
         raise ValueError('LinkedList.remove(x): x not in LinkedList')
 
     def pop(self, pos=-1):
@@ -131,17 +131,18 @@ class LinkedList:
         :return: 移除的节点
         """
         self._check_pos(pos)
-        prev = self._traverse(pos)
-        curr = prev.next
-        prev.next = curr.next
-        curr.next = None
+        previous_node = self._traverse(pos)
+        current_node = previous_node.next
+        previous_node.next = current_node.next
+        current_node.next = None
         self._length -= 1
-        return curr
+        return current_node
 
 
 class DoubleLinkedList(LinkedList):
 
-    def __init__(self, seq=None):
+    def __init__(self, seq=None, **kwargs):
+        super().__init__(**kwargs)
         self.head = DoubleNode(value=None)
         self.tail = DoubleNode(value=None)
         if seq is None:
@@ -151,28 +152,80 @@ class DoubleLinkedList(LinkedList):
         else:
             seq_length = len(seq)
             self._length = seq_length
-            self.head.next = curr = DoubleNode(value=seq[0])
-            prev = self.head
+            self.head.next = current_node = DoubleNode(value=seq[0])
+            previous_node = self.head
             for i in range(1, seq_length):
-                curr.next = DoubleNode(value=seq[i])
-                curr.prev = prev
-                prev = curr
-                curr = curr.next
-            curr.next = self.tail
-            self.tail.prev = curr
+                current_node.next = DoubleNode(value=seq[i])
+                current_node.prev = previous_node
+                previous_node = current_node
+                current_node = current_node.next
+            current_node.next = self.tail
+            self.tail.prev = current_node
 
     def __str__(self):
-        curr = self.head
+        current_node = self.head
         string = "【head】"
-        while curr.next.next is not None:
-            curr = curr.next
-            string += f"<=>【{curr.value}】"
+        while current_node.next.next is not None:
+            current_node = current_node.next
+            string += f"<=>【{current_node.value}】"
         return string + "<=>【tail】"
 
     def append(self, value):
-        curr = DoubleNode(value=value)
-        self.tail.prev.next = curr
-        curr.prev = self.tail.prev
-        self.tail.prev = curr
-        curr.next = self.tail
+        """
+        在链表尾部插入一个节点
+        :param value: 要插入节点的值
+        """
+        current_node = DoubleNode(value=value)
+        self.tail.prev.next = current_node
+        current_node.prev = self.tail.prev
+        self.tail.prev = current_node
+        current_node.next = self.tail
         self._length += 1
+
+    def insert(self, pos, value):
+        """
+        在链表指定位置 pos 处的节点前插入一个节点
+        :param pos: 链表中节点的位置
+        :param value: 要插入节点的值
+        """
+        self._check_pos(pos, method_type=MethodType.INSERT)
+        node = DoubleNode(value=value)
+        previous_node = self._traverse(pos)
+        previous_node.next.prev = node
+        node.prev = previous_node
+        node.next = previous_node.next
+        previous_node.next = node
+        self._length += 1
+
+    def remove(self, value):
+        """
+        移除与值 value 匹配的第一个节点
+        :param value: 要移除节点的值
+        """
+        previous_node = self.head
+        for node in self:
+            if node.value == value:
+                node.next.prev = previous_node
+                node.prev = None
+                previous_node.next = node.next
+                node.next = None
+                self._length -= 1
+                return
+            previous_node = node
+        raise ValueError('LinkedList.remove(x): x not in LinkedList')
+
+    def pop(self, pos=-1):
+        """
+        移除指定位置的节点并返回，默认移除最后一个节点
+        :param pos: 链表中节点的位置
+        :return: 移除的节点
+        """
+        self._check_pos(pos)
+        previous_node = self._traverse(pos)
+        current_node = previous_node.next
+        current_node.next.prev = previous_node
+        current_node.prev = None
+        previous_node.next = current_node.next
+        current_node.next = None
+        self._length -= 1
+        return current_node
