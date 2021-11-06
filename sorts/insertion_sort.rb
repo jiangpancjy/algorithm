3269a5b24f0cee10f29d62ec1a2e90bc0ea88d99
jiangyueczr <jiangpancjy@qq.com>
2021-11-06 13:04:45 +0000

    feat: 使用 ruby 实现插入排序的第二种方法

--- sorts/insertion_sort.rb
+++ sorts/insertion_sort.rb
@@ -15,3 +15,21 @@ def insertion_sort(array)
     end
     return array
 end
+
+def insertion_sort2(array)
+    # 插入排序算法的代码实现-升级版
+    # :param array: 一个数组
+    # :return: 一个升序数组
+    for i in 0...(array.length - 1)
+        temp_index = i
+        insert_value = array[i + 1]
+        while i >= 0 and insert_value < array[i] do
+            array[i + 1] = array[i]
+            i -= 1
+        end
+        if i != temp_index
+            array[i + 1] = insert_value
+        end
+    end
+    return array
+end
